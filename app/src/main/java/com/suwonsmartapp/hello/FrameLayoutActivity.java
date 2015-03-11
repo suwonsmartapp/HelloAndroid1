bfdf08b1a5750220a3533bd5e5f7369c9e61bcc1
junsuk Oh <a811219@gmail.com>
2015-03-11 16:33:13 +0900

    FrameLayout 연습 - ImageView 2장 겹쳐서 Visibility 변경하는 예제

---
+++ app/src/main/java/com/suwonsmartapp/hello/FrameLayoutActivity.java
@@ -0,0 +1,63 @@
+
+package com.suwonsmartapp.hello;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class FrameLayoutActivity extends ActionBarActivity {
+
+    Button mChangeButton;
+    ImageView mImage1;
+    ImageView mImage2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_layout);
+
+        mChangeButton = (Button)findViewById(R.id.changeBtn);
+        mImage1 = (ImageView)findViewById(R.id.image1);
+        mImage2 = (ImageView)findViewById(R.id.image2);
+
+        mChangeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mImage1.getVisibility() == View.VISIBLE) {
+                    mImage1.setVisibility(View.INVISIBLE);
+                    mImage2.setVisibility(View.VISIBLE);
+                } else {
+                    mImage1.setVisibility(View.VISIBLE);
+                    mImage2.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_frame_layout, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
