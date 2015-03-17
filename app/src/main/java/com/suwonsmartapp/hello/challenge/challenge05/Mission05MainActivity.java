106ab146299b0ea55fd43cff5d7fe4da691ea9db
Junsuk Oh <a811219@gmail.com>
2015-03-17 23:21:53 +0900

    도전05. 고객 정보 입력 화면의 구성 - p.332

--- app/src/main/java/com/suwonsmartapp/hello/challenge/challenge05/Mission05MainActivity.java
+++ app/src/main/java/com/suwonsmartapp/hello/challenge/challenge05/Mission05MainActivity.java
@@ -1,17 +1,85 @@
+
 package com.suwonsmartapp.hello.challenge.challenge05;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.suwonsmartapp.hello.R;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 public class Mission05MainActivity extends ActionBarActivity {
+
+    private EditText mNameEditText;
+    private EditText mAgeEditText;
+    private Button mSaveBtn;
+    private Button mBirthdayBtn;
+
+    private int year, month, day, hour, minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mission05_main);
 
+        mNameEditText = (EditText) findViewById(R.id.textView_name);
+        mAgeEditText = (EditText) findViewById(R.id.textView_age);
+        mBirthdayBtn = (Button) findViewById(R.id.btn_birthday);
+        mSaveBtn = (Button) findViewById(R.id.btn_save);
+
+        GregorianCalendar calendar = new GregorianCalendar();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        hour = calendar.get(Calendar.HOUR_OF_DAY);
+        minute = calendar.get(Calendar.MINUTE);
+        mBirthdayBtn.setText(year + "년 " + (month + 1) + "월 " + day + "일");
     }
+
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                int dayOfMonth) {
+            // String msg = String.format("%d 년 %d 월 %d 일", year, monthOfYear +
+            // 1, dayOfMonth);
+            SimpleDateFormat sf = new SimpleDateFormat("yyyy년 MM월 dd일");
+            GregorianCalendar tmpCalendar = new GregorianCalendar();
+            tmpCalendar.set(year, monthOfYear, dayOfMonth);
+
+            mBirthdayBtn.setText(sf.format(tmpCalendar.getTime()));
+            Toast.makeText(getApplicationContext(), sf.format(tmpCalendar.getTime()),
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    // TimePickerDialog 참고
+    /*private TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            String am_pm = "";
+
+            Calendar datetime = Calendar.getInstance();
+            datetime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+            datetime.set(Calendar.MINUTE, minute);
+
+            if (datetime.get(Calendar.AM_PM) == Calendar.AM)
+                am_pm = "AM";
+            else if (datetime.get(Calendar.AM_PM) == Calendar.PM)
+                am_pm = "PM";
+
+            String strHrsToShow = (datetime.get(Calendar.HOUR) == 0) ? "12" : datetime
+                    .get(Calendar.HOUR) + "";
+
+            strHrsToShow += " 시 " + datetime.get(Calendar.MINUTE) + " 분 " + am_pm;
+        }
+    };*/
 
 }
