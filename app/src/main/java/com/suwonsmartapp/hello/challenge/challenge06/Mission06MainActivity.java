f452264dff55e526edff1f7e88fd4b5b517091f6
Junsuk Oh <a811219@gmail.com>
2015-03-17 17:00:47 +0900

    도전.06 웹브라우저 화면 구성

---
+++ app/src/main/java/com/suwonsmartapp/hello/challenge/challenge06/Mission06MainActivity.java
@@ -0,0 +1,59 @@
+
+package com.suwonsmartapp.hello.challenge.challenge06;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import com.suwonsmartapp.hello.R;
+
+public class Mission06MainActivity extends ActionBarActivity {
+
+    private Button mShowAddressBtn;
+    private Button mShowWebViewBtn;
+
+    private LinearLayout mAddressLayout;
+    private EditText mAddressEditText;
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mission06_main);
+
+        mShowAddressBtn = (Button) findViewById(R.id.showAddressBtn);
+        mShowWebViewBtn = (Button) findViewById(R.id.showWebViewBtn);
+        mAddressLayout = (LinearLayout) findViewById(R.id.addressLayout);
+        mAddressEditText = (EditText) findViewById(R.id.addressEditText);
+        mWebView = (WebView) findViewById(R.id.webview);
+
+        // 페이지 이동 시 새로운 창이 아닌 현재 webView 에 표시
+        mWebView.setWebViewClient(new WebViewClient());
+
+
+        mShowAddressBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAddressLayout.getVisibility() == View.VISIBLE) {
+                    mAddressLayout.setVisibility(View.GONE);
+                } else if (mAddressLayout.getVisibility() == View.GONE) {
+                    mAddressLayout.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        mShowWebViewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mWebView.loadUrl(mAddressEditText.getText().toString());
+            }
+        });
+    }
+
+}
