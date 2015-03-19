1cc5a262cda9615e1db9cb9ce8150674d5744f05
Junsuk Oh <a811219@gmail.com>
2015-03-19 10:12:17 +0900

    ListView 예제 - onItemClickListener 구현

--- app/src/main/java/com/suwonsmartapp/hello/listview/ListViewExam01Activity.java
+++ app/src/main/java/com/suwonsmartapp/hello/listview/ListViewExam01Activity.java
@@ -3,13 +3,19 @@ package com.suwonsmartapp.hello.listview;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.suwonsmartapp.hello.R;
 
 import java.util.ArrayList;
 
+/**
+ * page. 345 ~ 355
+ */
 public class ListViewExam01Activity extends ActionBarActivity {
 
     private ArrayList<String> mNameList;
@@ -21,7 +27,7 @@ public class ListViewExam01Activity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view_exam01);
 
-        mListView = (ListView)findViewById(R.id.listView);
+        mListView = (ListView) findViewById(R.id.listView);
 
         // Data 준비
         mNameList = new ArrayList<>();
@@ -31,13 +37,43 @@ public class ListViewExam01Activity extends ActionBarActivity {
         mNameList.add("배꽃그리고솔");
         mNameList.add("신남교");
         mNameList.add("현기웅");
+        mNameList.add("오준석");
+        mNameList.add("유준택");
+        mNameList.add("손상문");
+        mNameList.add("배꽃그리고솔");
+        mNameList.add("신남교");
+        mNameList.add("현기웅");
+        mNameList.add("오준석");
+        mNameList.add("유준택");
+        mNameList.add("손상문");
+        mNameList.add("배꽃그리고솔");
+        mNameList.add("신남교");
+        mNameList.add("현기웅");
+        mNameList.add("오준석");
+        mNameList.add("유준택");
+        mNameList.add("손상문");
+        mNameList.add("배꽃그리고솔");
+        mNameList.add("신남교");
+        mNameList.add("현기웅");
 
         // Adapter 준비
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
                 android.R.layout.simple_list_item_1, mNameList);
 
         // View에 붙이기
         mListView.setAdapter(adapter);
+
+        // 이벤트 처리
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(
+                        ListViewExam01Activity.this,
+                        "position : " + position + ", id : " + id + ", data->text : "
+                                + mNameList.get(position) + ", adapter->text : "
+                                + adapter.getItem(position), Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
