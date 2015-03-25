d712e6a7051cf667c514613a7926300b96f5b688
Junsuk Oh <a811219@gmail.com>
2015-03-25 15:37:40 +0900

    Thread 예제 2

--- app/src/main/java/com/suwonsmartapp/hello/thread/ThreadActivity.java
+++ app/src/main/java/com/suwonsmartapp/hello/thread/ThreadActivity.java
@@ -3,10 +3,12 @@ package com.suwonsmartapp.hello.thread;
 
 import com.suwonsmartapp.hello.R;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -34,11 +36,9 @@ public class ThreadActivity extends ActionBarActivity implements View.OnClickLis
     private Handler mProgressHandler2 = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            if (mProgressBar2.getProgress() < mProgressBar2.getMax()) {
-                mProgressBar2.incrementProgressBy(1);
-                mProgressTextView2.setText("진행률 : " + mProgressBar2.getProgress());
-                mProgressHandler2.sendEmptyMessageDelayed(0, 100);
-            }
+            // UI 업데이트
+            mProgressBar2.setProgress(msg.what);
+            mProgressTextView2.setText("진행률 : " + mProgressBar2.getProgress());
         }
     };
 
@@ -66,17 +66,18 @@ public class ThreadActivity extends ActionBarActivity implements View.OnClickLis
         switch (v.getId()) {
             case R.id.btn_thread:
                 // 위쪽 ProgressBar. 0.5초에 5 씩 증가하는 쓰레드
-                mProgressBar.setProgress(0);
-                Thread thread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        // 계산
-
-                        // UI 업데이트
-                        mProgressHandler.sendEmptyMessage(0);
-                    }
-                });
-                thread.start();
+                // mProgressBar.setProgress(0);
+                // Thread thread = new Thread(new Runnable() {
+                // @Override
+                // public void run() {
+                // // 계산
+                //
+                // // UI 업데이트
+                // mProgressHandler.sendEmptyMessage(0);
+                // }
+                // });
+                // thread.start();
+                new ProgressBarTask().execute();
 
                 // 아래쪽 ProgressBar. 0.1초에 1 씩 증가하는 쓰레드
                 mProgressBar2.setProgress(0);
@@ -84,9 +85,17 @@ public class ThreadActivity extends ActionBarActivity implements View.OnClickLis
                     @Override
                     public void run() {
                         // 계산
-
-                        // UI 업데이트
-                        mProgressHandler2.sendEmptyMessage(0);
+                        int progress = 0;
+                        while (mProgressBar.getProgress() < mProgressBar.getMax()) {
+                            progress += 1;
+                            // UI 업데이트
+                            mProgressHandler2.sendEmptyMessage(progress);
+                            try {
+                                Thread.sleep(100);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
                     }
                 });
                 thread2.start();
@@ -128,5 +137,48 @@ public class ThreadActivity extends ActionBarActivity implements View.OnClickLis
                 Toast.makeText(getApplicationContext(), "토스트", Toast.LENGTH_SHORT).show();
                 break;
         }
+    }
+
+    public class ProgressBarTask extends AsyncTask<Void, Integer, Void> {
+        private int progress;
+
+        // 쓰레드가 수행 되기 전 초기화 등
+        @Override
+        protected void onPreExecute() {
+            progress = 0;
+            mProgressBar.setProgress(progress);
+            Log.d(ProgressBarTask.class.getSimpleName(), "onPreExecute");
+        }
+
+        // background 에서 수행할 내용 (Thread 에서 수행할 내용)
+        @Override
+        protected Void doInBackground(Void... params) {
+            // Background 작업 조건
+            while (mProgressBar.getProgress() < mProgressBar.getMax()) {
+                Log.d(ProgressBarTask.class.getSimpleName(), "doInBackground");
+                progress += 5;
+
+                mProgressBar.setProgress(progress);
+
+                // onProgressUpdate 가 수행 되도록 하는 메소드
+                publishProgress(progress);
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        // UI 업데이트 (Handler 에서 수행할 내용)
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            mProgressTextView.setText("진행률 : " + values[0]);
+
+            Log.d(ProgressBarTask.class.getSimpleName(), "onProgressUpdate");
+        }
+
     }
 }
