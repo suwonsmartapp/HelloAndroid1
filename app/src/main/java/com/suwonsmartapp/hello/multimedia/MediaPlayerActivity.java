05204d585e47385d8aa0954bb2a4260e29a87b83
Junsuk Oh <a811219@gmail.com>
2015-04-27 10:54:13 +0900

    비디오 플레이어 예제

--- app/src/main/java/com/suwonsmartapp/hello/multimedia/MediaPlayerActivity.java
+++ app/src/main/java/com/suwonsmartapp/hello/multimedia/MediaPlayerActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.VideoView;
 
 import com.suwonsmartapp.hello.R;
 
@@ -17,12 +18,18 @@ import java.io.IOException;
  */
 public class MediaPlayerActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button mBtnFilePick;    // 파일 선택
+    private static final int REQUEST_CODE_AUDIO = 0;
+    private static final int REQUEST_CODE_VIDEO = 1;
+
+    private Button mBtnAudioFilePick;    // 파일 선택
+    private Button mBtnVideoFilePick;    // 파일 선택
     private Button mBtnPause;       // 정지
     private Button mBtnResume;      // 재시작
 
     // 플레이어
     private MediaPlayer mMediaPlayer;
+
+    private VideoView mVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,23 +41,33 @@ public class MediaPlayerActivity extends AppCompatActivity implements View.OnCli
     }
 
     private void init() {
-        mBtnFilePick = (Button) findViewById(R.id.btn_filePick);
+        mBtnAudioFilePick = (Button) findViewById(R.id.btn_audioFilePick);
+        mBtnVideoFilePick = (Button) findViewById(R.id.btn_videoFilePick);
         mBtnPause = (Button) findViewById(R.id.btn_pause);
         mBtnResume = (Button) findViewById(R.id.btn_resume);
+        mVideoView = (VideoView) findViewById(R.id.videoView);
 
-        mBtnFilePick.setOnClickListener(this);
+        mBtnAudioFilePick.setOnClickListener(this);
+        mBtnVideoFilePick.setOnClickListener(this);
         mBtnPause.setOnClickListener(this);
         mBtnResume.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        Intent i = null;
         switch (v.getId()) {
-            case R.id.btn_filePick:
+            case R.id.btn_audioFilePick:
                 // FileChooser 사용
-                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i = new Intent(Intent.ACTION_GET_CONTENT);
                 i.setType("audio/*");
-                startActivityForResult(Intent.createChooser(i, "파일선택..."), 0);
+                startActivityForResult(Intent.createChooser(i, "파일선택..."), REQUEST_CODE_AUDIO);
+                break;
+            case R.id.btn_videoFilePick:
+                // FileChooser 사용
+                i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.setType("video/*");
+                startActivityForResult(Intent.createChooser(i, "파일선택..."), REQUEST_CODE_VIDEO);
                 break;
             case R.id.btn_pause:
                 if (mMediaPlayer != null) {
@@ -69,7 +86,8 @@ public class MediaPlayerActivity extends AppCompatActivity implements View.OnCli
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == 0 && resultCode == RESULT_OK) {
+        if (requestCode == REQUEST_CODE_AUDIO && resultCode == RESULT_OK) {
+            // Audio
             Uri fileUri = data.getData();
             try {
                 if (mMediaPlayer != null) {
@@ -81,11 +99,19 @@ public class MediaPlayerActivity extends AppCompatActivity implements View.OnCli
                 mMediaPlayer.prepare();
                 mMediaPlayer.start();
 
-                mBtnFilePick.setText(fileUri.getPath());
+                mBtnAudioFilePick.setText(fileUri.getPath());
             } catch (IOException e) {
                 // IOException 이 났을 때 다른 익셉션을 발생시켜서 죽인다
                 throw new RuntimeException("io exception");
             }
+        } else if (requestCode == REQUEST_CODE_VIDEO && resultCode == RESULT_OK) {
+            // Video
+            Uri fileUri = data.getData();
+
+            mVideoView.setVideoURI(fileUri);
+            mVideoView.start();
+
+            mBtnVideoFilePick.setText(fileUri.getPath());
         }
     }
 }
