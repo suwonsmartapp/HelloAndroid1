abd7223b48b74443c08976fbb57a270cac6ef1da
Junsuk Oh <a811219@gmail.com>
2015-05-12 14:59:15 +0900

    - 뮤직 플레이어 수정 중 1. Service 에서 플레이 2. SeekBar 추가 3. 앨범 정보 취득

--- app/src/main/java/com/suwonsmartapp/hello/multimedia/MediaPlayerActivity.java
+++ app/src/main/java/com/suwonsmartapp/hello/multimedia/MediaPlayerActivity.java
@@ -1,37 +1,63 @@
 package com.suwonsmartapp.hello.multimedia;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.graphics.BitmapFactory;
+import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.suwonsmartapp.hello.R;
 
-import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * 챕터 9. 멀티미디어
  */
-public class MediaPlayerActivity extends AppCompatActivity implements View.OnClickListener {
+public class MediaPlayerActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener, Chronometer.OnChronometerTickListener {
 
     private static final int REQUEST_CODE_AUDIO = 0;
     private static final int REQUEST_CODE_VIDEO = 1;
+    private static final String TAG = MediaPlayerActivity.class.getSimpleName();
 
     private Button mBtnAudioFilePick;    // 파일 선택
     private Button mBtnVideoFilePick;    // 파일 선택
     private Button mBtnPause;       // 정지
     private Button mBtnResume;      // 재시작
 
+    private TextView mTvArtist;
+    private TextView mTvAlbum;
+    private TextView mTvTitle;
+    private Chronometer mPlayTime;
+    private TextView mTvDuration;
+    private SeekBar mSeekBar;
+    private ImageView mIvAlbutArt;
+
     // 플레이어
     private MediaPlayer mMediaPlayer;
 
     private VideoView mVideoView;
+    private long mStartTime = 0;
+    private long mStopTime = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +66,10 @@ public class MediaPlayerActivity extends AppCompatActivity implements View.OnCli
 
         // 레이아웃 초기화, 이벤트 연결
         init();
+
+        // 서비스에서 정보를 가져오기 위해 바인드
+        Intent intent = new Intent(getApplicationContext(), MusicService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
 
         // 다른 앱에서 호출 될 때의 처리
         if (getIntent() != null) {
@@ -57,10 +87,20 @@ public class MediaPlayerActivity extends AppCompatActivity implements View.OnCli
         mBtnResume = (Button) findViewById(R.id.btn_resume);
         mVideoView = (VideoView) findViewById(R.id.videoView);
 
+        mTvArtist = (TextView) findViewById(R.id.tv_artist);
+        mTvAlbum = (TextView) findViewById(R.id.tv_album);
+        mTvTitle = (TextView) findViewById(R.id.tv_title);
+        mPlayTime = (Chronometer) findViewById(R.id.chronometer_play_time);
+        mTvDuration = (TextView) findViewById(R.id.tv_duration);
+        mSeekBar = (SeekBar) findViewById(R.id.seekbar_play_time);
+        mIvAlbutArt = (ImageView) findViewById(R.id.iv_albumArt);
+
         mBtnAudioFilePick.setOnClickListener(this);
         mBtnVideoFilePick.setOnClickListener(this);
         mBtnPause.setOnClickListener(this);
         mBtnResume.setOnClickListener(this);
+        mSeekBar.setOnSeekBarChangeListener(this);
+        mPlayTime.setOnChronometerTickListener(this);
     }
 
     @Override
@@ -83,13 +123,28 @@ public class MediaPlayerActivity extends AppCompatActivity implements View.OnCli
                 if (mMediaPlayer != null) {
                     mMediaPlayer.pause();
                 }
+                if (mService != null) {
+                    Log.d(TAG, "서비스에서 가져온 값 : " + mService.getCurrentPosition());
+                }
+                startService(getMusicServiceIntent(MusicService.ACTION_PAUSE));
+                mPlayTime.stop();
+                mStopTime = SystemClock.elapsedRealtime();
                 break;
             case R.id.btn_resume:
                 if (mMediaPlayer != null) {
                     mMediaPlayer.start();
                 }
+                startService(getMusicServiceIntent(MusicService.ACTION_RESUME));
+                mPlayTime.setBase(SystemClock.elapsedRealtime() - mStopTime + mStartTime);
+                mPlayTime.start();
                 break;
         }
+    }
+
+    private Intent getMusicServiceIntent(String action) {
+        Intent intent = new Intent(getApplicationContext(), MusicService.class);
+        intent.setAction(action);
+        return intent;
     }
 
     @Override
@@ -100,14 +155,17 @@ public class MediaPlayerActivity extends AppCompatActivity implements View.OnCli
             // Audio
             Uri fileUri = data.getData();
 
-//            Intent intent = new Intent(getApplicationContext(), MusicService.class);
-//            intent.putExtra("uri", fileUri);
-//            startService(intent);
+            // 정보 셋팅
+            setMetaInfo(fileUri);
 
+            //　음악 재생
             startMusic(fileUri);
         } else if (requestCode == REQUEST_CODE_VIDEO && resultCode == RESULT_OK) {
             // Video
             Uri fileUri = data.getData();
+
+            mIvAlbutArt.setVisibility(View.INVISIBLE);
+            mVideoView.setVisibility(View.VISIBLE);
 
             mVideoView.setVideoURI(fileUri);
             mVideoView.start();
@@ -116,22 +174,100 @@ public class MediaPlayerActivity extends AppCompatActivity implements View.OnCli
         }
     }
 
-    private void startMusic(Uri fileUri) {
-        if (mMediaPlayer != null) {
-            mMediaPlayer.release();
-            mMediaPlayer = null;
-        }
+    private SimpleDateFormat mTimeFormat = new SimpleDateFormat("mm:ss");
 
-        try {
-            mMediaPlayer = new MediaPlayer();
-            mMediaPlayer.setDataSource(getApplicationContext(), fileUri);
-            mMediaPlayer.prepare();
-            mMediaPlayer.start();
-        } catch (IOException e) {
-            e.printStackTrace();
+    private void setMetaInfo(Uri fileUri) {
+        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+        retriever.setDataSource(getApplicationContext(), fileUri);
+        mTvAlbum.setText(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM));
+        mTvTitle.setText(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE));
+        mTvArtist.setText(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST));
+
+        String duration = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+
+        mTvDuration.setText(mTimeFormat.format(new Date(Long.valueOf(duration))));
+        mSeekBar.setMax(Integer.valueOf(duration));
+
+        // SeekBar 갱신
+        mHandler.post(mUpdateSeekBarRunnable);
+
+        // 앨범 사진
+        byte data[] = retriever.getEmbeddedPicture();
+        if (null != data) {
+            mVideoView.setVisibility(View.INVISIBLE);
+            mIvAlbutArt.setVisibility(View.VISIBLE);
+            mIvAlbutArt.setImageBitmap(BitmapFactory.decodeByteArray(data, 0, data.length));
         }
+    }
+
+    private void startMusic(Uri fileUri) {
+//        if (mMediaPlayer != null) {
+//            mMediaPlayer.release();
+//            mMediaPlayer = null;
+//        }
+//
+//        try {
+//            mMediaPlayer = new MediaPlayer();
+//            mMediaPlayer.setDataSource(getApplicationContext(), fileUri);
+//            mMediaPlayer.prepare();
+//            mMediaPlayer.start();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+        // 음악 재생
+        Intent intent = new Intent(getApplicationContext(), MusicService.class);
+        intent.setAction(MusicService.ACTION_START);
+        intent.putExtra("uri", fileUri);
+        startService(intent);
 
         mBtnAudioFilePick.setText(fileUri.getPath());
+
+//        Uri albumUri = ContentUris.withAppendedId(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI, fileUri);
+//
+//        Cursor cursor = getContentResolver().query(fileUri,
+//                null,
+//                null,
+//                null,
+//                null);
+//        cursor.moveToFirst();
+//        mTvArtist.setText(cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.ARTIST)));
+//        mTvAlbum.setText(cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.ALBUM)));
+//        mTvTitle.setText(cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.TITLE)));
+//        cursor.close();
+
+        mPlayTime.setBase(mStartTime = SystemClock.elapsedRealtime());
+        mPlayTime.start();
+    }
+
+    private MusicService mService;
+    private boolean mBound = false;
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            MusicService.MusicServiceBinder binder = (MusicService.MusicServiceBinder) service;
+            mService = binder.getService();
+            mBound = true;
+            Log.d(TAG, "MusinService connected");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
     }
 
     @Override
@@ -143,5 +279,57 @@ public class MediaPlayerActivity extends AppCompatActivity implements View.OnCli
         }
 
         return super.onKeyDown(keyCode, event);
+    }
+
+
+    public static final String BROADCAST_START_MUSIC = "BROADCAST_START_MUSIC";
+
+    private Handler mHandler = new Handler();
+
+    private Runnable mUpdateSeekBarRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (mBound && mService != null) {
+                mSeekBar.setProgress(mService.getCurrentPosition());
+                mHandler.postDelayed(this, 1000);
+            }
+        }
+    };
+
+    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+
+                }
+            });
+        }
+    };
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        if (mService != null) {
+            mService.setCurrentPosition(seekBar.getProgress());
+            mPlayTime.setBase(mStartTime - seekBar.getProgress());
+            mPlayTime.start();
+        }
+    }
+
+    @Override
+    public void onChronometerTick(Chronometer chronometer) {
+        if (chronometer.getText().equals(mTvDuration.getText())) {
+            mPlayTime.stop();
+        }
     }
 }
--- app/src/main/java/com/suwonsmartapp/hello/multimedia/MusicService.java
+++ app/src/main/java/com/suwonsmartapp/hello/multimedia/MusicService.java
@@ -4,13 +4,29 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
 import java.io.IOException;
 
 public class MusicService extends Service {
+    private static final String TAG = MusicService.class.getSimpleName();
+
+    public static final String ACTION_PAUSE = "action_pause";
+    public static final String ACTION_RESUME = "action_resume";
+    public static final String ACTION_START = "action_start";
+
     // 플레이어
     private MediaPlayer mMediaPlayer;
+
+    private final IBinder mBinder = new MusicServiceBinder();
+
+    public class MusicServiceBinder extends Binder {
+        public MusicService getService() {
+            return MusicService.this;
+        }
+    }
 
     public MusicService() {
     }
@@ -24,25 +40,63 @@ public class MusicService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent == null) {
+            return super.onStartCommand(intent, flags, startId);
+        }
+
+        String action = intent.getAction();
+        switch (action) {
+            case ACTION_START:
+                startMusic(intent);
+                break;
+            case ACTION_PAUSE:
+                pauseMusic();
+                break;
+            case ACTION_RESUME:
+                resumeMusic();
+                break;
+        }
+
+        return START_NOT_STICKY;
+    }
+
+    private Intent getBroadCastIntent() {
+        return new Intent(MediaPlayerActivity.BROADCAST_START_MUSIC);
+    }
+
+    public void startMusic(Intent intent) {
         if (mMediaPlayer != null) {
             mMediaPlayer.release();
             mMediaPlayer = null;
         }
 
-        try {
-            if (intent == null) {
-                return super.onStartCommand(intent, flags, startId);
-            }
-            Uri fileUri = intent.getParcelableExtra("uri");
+        Uri fileUri = intent.getParcelableExtra("uri");
 
+        try {
             mMediaPlayer = new MediaPlayer();
             mMediaPlayer.setDataSource(MusicService.this, fileUri);
-            mMediaPlayer.prepare();
-            mMediaPlayer.start();
+            mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
+            mMediaPlayer.prepareAsync();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return super.onStartCommand(intent, flags, startId);
+    }
+
+    public void pauseMusic() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.pause();
+        }
+    }
+
+    public void resumeMusic() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.start();
+        }
     }
 
     @Override
@@ -56,7 +110,21 @@ public class MusicService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Log.d(TAG, "onBind");
+        return mBinder;
     }
+
+    public int getCurrentPosition() {
+        if (mMediaPlayer == null) {
+            return -1;
+        }
+        return mMediaPlayer.getCurrentPosition();
+    }
+
+    public void setCurrentPosition(int position) {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.seekTo(position);
+        }
+    }
+
 }
