16aeda24f9baaff9497e5aa5d7ab63f788819a1c
Junsuk Oh <a811219@gmail.com>
2015-06-17 10:35:01 +0900

    AppbarLayout 주석 추가, 소스 수정

--- app/src/main/java/com/suwonsmartapp/hello/activity/AppbarLayoutFragment.java
+++ app/src/main/java/com/suwonsmartapp/hello/activity/AppbarLayoutFragment.java
@@ -3,10 +3,11 @@ package com.suwonsmartapp.hello.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.suwonsmartapp.hello.R;
 
@@ -19,9 +20,16 @@ public class AppbarLayoutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_coordinator, container, false);
 
-        Toolbar toolbar = (Toolbar)rootView.findViewById(R.id.toolbar);
-        toolbar.setTitle("Coordinator");
+//        Toolbar toolbar = (Toolbar)rootView.findViewById(R.id.toolbar);
+//        toolbar.setTitle("Coordinator");
 
+        LinearLayout layout = (LinearLayout)rootView.findViewById(R.id.contents);
+
+        for (int i = 0; i < 30; i++) {
+            TextView tv = new TextView(getActivity());
+            tv.setText("Item " + i);
+            layout.addView(tv);
+        }
 
 
         return rootView;
